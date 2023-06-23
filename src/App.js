e9d60bccf4799cc6616585a16d9e32820a380292
Hilll19 <hilworking0110@gmail.com>
2023-06-23 22:04:48 +0700

    add wrapper api, search, and useEffect/State

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getMovieList, searchMovie } from "./api.js";
 
 const App = () => {
-  const search = (q) => {
-    console.log({q})
-  }
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+          <div className="Movie-date">release : {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const search = async(q) => {
+    if(q.length > 3){
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    } 
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Cinema</h1>
-        <input 
-          placeholder="cari movie..." 
-          className="Movie-search" 
-          onChange={({target}) => search(target.value)}
-
+        <input
+          placeholder="cari movie..."
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
         />
         <div className="Movie-container">
-          <div className="Movie-wrapper">
-            <div className="Movie-title">Contoh Pertama</div>
-            <img src="" alt="" className="Movie-image" />
-            <div className="Movie-date">11-12-2023</div>
-            <div className="Movie-rate">8.9</div>
-          </div>
+          <PopularMovieList />
         </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
