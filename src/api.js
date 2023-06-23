a69fc9752d99026e62fac8d850518a094e5f0126
Hilll19 <hilworking0110@gmail.com>
2023-06-23 16:50:35 +0700

    for export api can direct to app.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,14 @@
-import axios from "axios";
+import axios from "axios"
 
-const getMovieLIst = async() => {
-    
+const apiKey = process.env.REACT_APP_APIKEY
+const baseUrl = process.env.REACT_APP_BASEURL
+
+export const getMovieList = async() => {
+    const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
+    return movie.data.results
+}
+
+export const searchMovie = async() => {
+    const search = await axios.get("")
+    return
 }
